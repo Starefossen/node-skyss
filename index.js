2ec64be05b3920ba6ea2ccc96d85badec3a7dfe8
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-07 22:54:09 +0100

    feat(api): add support for stop API enpoints

--- index.js
+++ index.js
@@ -32,3 +32,43 @@ module.exports.mobile.messages = function messages(cb) {
   const url = `${skyssMobileApiUrl}/messages`;
   jsonist.get(url, module.exports._opts(), cb);
 };
+
+/**
+ * Get list of transport stops
+ *
+ * @param opts - query options {rows=1000, q=undefined}
+ * @param cb - callback function (err, data, res)
+ *
+ * @return Object with stops
+ */
+module.exports.mobile.stops = function stops(opts, cb) {
+  const qs = opts;
+
+  qs.rows = qs.rows || 1000;
+
+  if (qs.q) { qs.q = encodeURIComponent(qs.q); }
+
+  const url = `${skyssMobileApiUrl}/stopgroups?${querystring.stringify(qs)}`;
+  jsonist.get(url, module.exports._opts(), cb);
+};
+
+/**
+ * Get transport stop details
+ *
+ * @param id - String with stop identifier
+ * @param opts - Object with query options {expand, stop}
+ *    expand=Stops.RouteDirections.PassingTimes
+ *    expand=Stops.RouteDirections.Notes
+ * @param cb - callback function (err, data, res)
+ *
+ * @return Object of stop details
+ */
+module.exports.mobile.stop = function stop(id, opts, cb) {
+  let url = `${skyssMobileApiUrl}/stopgroups/${id}`;
+
+  if (opts.expand) {
+    url += `?expand=${opts.expand.join('&expand=')}`;
+  }
+
+  jsonist.get(url, module.exports._opts(), cb);
+};
--- test/mobile.js
+++ test/mobile.js
@@ -26,3 +26,130 @@ describe('messages()', function describe() {
     });
   });
 });
+
+describe('stops()', function describe() {
+  it('returns a list of stops', function it(done) {
+    this.timeout(10000);
+
+    const opts = {
+      rows: 10,
+    };
+
+    skyss.mobile.stops(opts, (err, data, resp) => {
+      assert.ifError(err);
+      assert.equal(resp.statusCode, 200);
+
+      const schema = Joi.object().keys({
+        Result: Joi.object().keys({
+          Found: Joi.number(),
+          Returned: Joi.number(),
+          Start: Joi.number(),
+          'Last-Modified': Joi.date(),
+        }),
+        StopGroups: Joi.array().items(Joi.object().keys({
+          Identifier: Joi.string(),
+          Description: Joi.string(),
+          ServiceModes: Joi.array().items(Joi.string()),
+          Location: Joi.string(),
+        })),
+        resultCode: Joi.string(),
+        errorCode: Joi.string().allow(null),
+        errorMessage: Joi.string().allow(null),
+      });
+
+      Joi.validate(data, schema, done);
+    });
+  });
+
+  it('returns a list of stops for search query');
+});
+
+describe('stop()', function describe() {
+  it('returns details for stop', function it(done) {
+    this.timeout(10000);
+
+    const id = 'groupId_123762465';
+    const opts = {};
+
+    skyss.mobile.stop(id, opts, (err, data, resp) => {
+      assert.ifError(err);
+      assert.equal(resp.statusCode, 200);
+
+      const schema = Joi.object().keys({
+        Result: Joi.object().keys({
+          Found: Joi.number(),
+          Returned: Joi.number(),
+          Start: Joi.number(),
+          'Last-Modified': Joi.date(),
+        }),
+        StopGroups: Joi.array().items(Joi.object().keys({
+          Identifier: Joi.string(),
+          Description: Joi.string(),
+          ServiceModes: Joi.array().items(Joi.string()),
+          Location: Joi.string(),
+        })),
+        resultCode: Joi.string(),
+        errorCode: Joi.string().allow(null),
+        errorMessage: Joi.string().allow(null),
+      });
+
+      Joi.validate(data, schema, done);
+    });
+  });
+
+  it('returns details for stop with expanded fields', function it(done) {
+    const id = 'groupId_123762465';
+    const opts = {
+      expand: [
+        'Stops.RouteDirections.PassingTimes',
+        'Stops.RouteDirections.Notes',
+      ],
+    };
+
+    skyss.mobile.stop(id, opts, (err, data, resp) => {
+      assert.ifError(err);
+      assert.equal(resp.statusCode, 200);
+
+      const schema = Joi.object().keys({
+        Result: Joi.object().keys({
+          Found: Joi.number(),
+          Returned: Joi.number(),
+          Start: Joi.number(),
+          'Last-Modified': Joi.date(),
+        }),
+        StopGroups: Joi.array().items(Joi.object().keys({
+          Identifier: Joi.string(),
+          Description: Joi.string(),
+          ServiceModes: Joi.array().items(Joi.string()),
+          Location: Joi.string(),
+          Stops: Joi.array().items(Joi.object().keys({
+            Identifier: Joi.string(),
+            Description: Joi.string(),
+            Platform: Joi.string().allow(''),
+            Location: Joi.string(),
+            ServiceModes: Joi.array().items(Joi.string()),
+            Detail: Joi.string(),
+            RouteDirections: Joi.array().items(Joi.object().keys({
+              PublicIdentifier: Joi.string(),
+              Description: Joi.string(),
+              Direction: Joi.string(),
+              DirectionName: Joi.string(),
+              ServiceMode: Joi.string(),
+              Identifier: Joi.string(),
+              PassingTimes: Joi.array(),
+              Notes: Joi.array().items(Joi.object().keys({
+                NoteCode: Joi.string(),
+                Text: Joi.string(),
+              })),
+            })),
+          })),
+        })),
+        resultCode: Joi.string(),
+        errorCode: Joi.string().allow(null),
+        errorMessage: Joi.string().allow(null),
+      });
+
+      Joi.validate(data, schema, done);
+    });
+  });
+});
